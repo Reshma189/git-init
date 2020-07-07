23a8c0115f7b69d3b5a0b491c9d0fb385f6d0d1a
Reshma.H <reshmavedu1989@gmail.com>
2020-07-07 22:25:01 +0530

    first commit

---
+++ app/src/main/java/com/example/zoomkart/BakeryActivity.java
@@ -0,0 +1,14 @@
+package com.example.zoomkart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BakeryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bakery);
+    }
+}
